9c7b359105eb513bbec3887873047d14ff564d77
Armand Kaufmann <armandkaufmann@gmail.com>
2022-10-23 23:14:38 -0300

    worked on formatting of text color based on logic

--- app/src/main/java/com/example/quizbuilder/Activity_3.java
+++ app/src/main/java/com/example/quizbuilder/Activity_3.java
@@ -2,8 +2,11 @@ package com.example.quizbuilder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -31,6 +34,9 @@ public class Activity_3 extends AppCompatActivity {
     TextView textViewHighestStreakNum;
     TextView textViewDifficultyText;
 
+    Button buttonRetry;
+    Button buttonChangeDiff;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +58,11 @@ public class Activity_3 extends AppCompatActivity {
         textViewTimePerQuestionNum = findViewById(R.id.textViewTimePerQuestionNum);
         textViewHighestStreakNum = findViewById(R.id.textViewHighestStreakNum);
         textViewDifficultyText = findViewById(R.id.textViewDifficultyText);
+
+        buttonRetry = findViewById(R.id.buttonRetry);
+        buttonRetry.setOnClickListener(buttonRetryListener);
+        buttonChangeDiff = findViewById(R.id.buttonChangeDiff);
+        buttonChangeDiff.setOnClickListener(buttonChangeDiffListener);
 
         //setting text based on bundle data
         getTimePerQuestion();
@@ -76,10 +87,11 @@ public class Activity_3 extends AppCompatActivity {
         timePerQuestion = sum / timePerQuestionHistory.size();
     }//end getTimePerQuestion method
 
+    //methods ======================================================================================
     private void getDifficultySetText(){
         if (level == Level.EASY){
             textViewDifficultyText.setText(getResources().getString(R.string.easy));
-            textViewDifficultyText.setTextColor(Color.parseColor("#64f575")); //green
+            textViewDifficultyText.setTextColor(Color.parseColor("#3CFA52")); //green
         }else if (level == Level.MEDIUM){
             textViewDifficultyText.setText(getResources().getString(R.string.Medium));
             textViewDifficultyText.setTextColor(Color.parseColor("#f18e00")); //orange
@@ -106,10 +118,14 @@ public class Activity_3 extends AppCompatActivity {
     private void getRatingToOutput(){
         if (percentageCorrect <= 50){
             textViewResultsRating.setText(getResources().getString(R.string.ratingBad));
+            textViewResultsRating.setTextColor(Color.parseColor("#f56464"));
+            textViewScorePercentage.setTextColor(Color.parseColor("#f56464"));
         }else if (percentageCorrect <= 90){
             textViewResultsRating.setText(getResources().getString(R.string.ratingGood));
+            textViewResultsRating.setTextColor(Color.parseColor("#3CFA52"));
         }else{
             textViewResultsRating.setText(getResources().getString(R.string.ratingPerfect));
+            textViewResultsRating.setTextColor(Color.parseColor("#3CFA52"));
         }
     }//end getRatingToOutput method
 
@@ -117,11 +133,11 @@ public class Activity_3 extends AppCompatActivity {
         String text = String.format("%.2f", percentageCorrect);
         String textFinal = text.concat("%");
         textViewScorePercentage.setText(textFinal);
-    }
+    }//end setScore
 
     private void setHighestStreak(){
         textViewHighestStreakNum.setText(Integer.toString(highestStreak));
-    }
+    }//end setHighestStreak
 
     private void setTimePerQuestion(){
         int minutes = (int) (timePerQuestion / 1000) / 60; //if adding minutes
@@ -130,5 +146,24 @@ public class Activity_3 extends AppCompatActivity {
         String timeLeftFormatted = String.format("%02d:%02d", minutes,seconds);
 
         textViewTimePerQuestionNum.setText(timeLeftFormatted);
-    }
+    }//end setTimePerQuestion
+
+    //listeners ====================================================================================
+    private View.OnClickListener buttonChangeDiffListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(Activity_3.this, MainActivity.class);
+            startActivity(i);
+        }
+    }; //end buttonNextQuestionListener
+
+    private View.OnClickListener buttonRetryListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(Activity_3.this, Activity_2.class);
+            i.putExtra("Level", level);
+            i.putExtra("DIFFICULTY", DIFFICULTY_TIME);
+            startActivity(i);
+        }
+    };//end
 }
